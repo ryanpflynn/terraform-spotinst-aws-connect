68086afc18986cb5ca156df9b6366b6a5cee2af3
Ryan Flynn <mr.ryanflynn@gmail.com>
2022-05-31 13:54:56 -0400

    Add user flag to pip command

--- scripts/spot-account-aws-wrapper.sh
+++ scripts/spot-account-aws-wrapper.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" && \
 cd "${DIR}"  && \
-pip3 install -r requirements.txt ||
+pip3 install --user -r requirements.txt ||
 { echo "Error installing python required modules"; exit 1; }
 
 python3 spot_account_aws.py "$@"
